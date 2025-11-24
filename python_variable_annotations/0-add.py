917982e5b4a8d6258704c33ba9acdf669d364360
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 <11534@holbertonstudents.com>
2025-11-24 14:56:58 +0100

    fix comments

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -4,5 +4,8 @@ Module that defines the add function.
 """
 
 def add(a: float, b: float) -> float:
-    """Return the sum of a and b."""
+    """
+    Return the sum of two floats a and b.
+    This function performs a basic arithmetic addition.
+    """
     return a + b
