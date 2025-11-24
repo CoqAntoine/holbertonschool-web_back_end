4f74d2168ae6b1b216ac84f5dfec9457b3df65a5
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 <11534@holbertonstudents.com>
2025-11-24 15:22:26 +0100

    add task 1

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module that defines the concat function.
+"""
+
+
+def concat(a: str, b: str) -> str:
+    """
+    Return the concatenation of a and b.
+    This function performs a basic arithmetic addition.
+    """
+    return a + b
