3b97bce0f3aae881309ba526c441c8928fb91e81
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 <11534@holbertonstudents.com>
2025-12-01 13:51:57 +0100

    task 1

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,6 +1,6 @@
 import csv
-import math
 from typing import List
+index_range = __import__('0-simple_helper_function').index_range
 
 
 class Server:
@@ -30,20 +30,3 @@ class Server:
         if start >= len(data):
             return []
         return data[start:end]
-
-
-def index_range(page, page_size):
-    """
-    Calculate the start and end index for a given pagination page.
-
-    Args:
-        page (int): The current page number (1-indexed).
-        page_size (int): The number of items per page.
-
-    Returns:
-        tuple: A tuple of two integers containing the start index (inclusive)
-            and the end index (exclusive) for the items on the given page.
-    """
-    start = (page - 1) * page_size
-    end = page * page_size
-    return (start, end)
