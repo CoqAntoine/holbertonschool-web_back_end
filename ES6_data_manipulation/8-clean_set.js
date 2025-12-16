f3f81cb61923bfb10dcd4f7a964db5fa329f7b38
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 <11534@holbertonstudents.com>
2025-12-16 15:04:47 +0100

    task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,8 @@
 export default function cleanSet(set, startString) {
+    if (!startString) return '';
+    if (typeof(startString) !== "string") return '';
     return Array.from(set)
         .filter(value => value.startsWith(startString))
         .map(value => value.slice(startString.length))
         .join('-');
 }
-    
