c6a0e0edd0cbbf7a023ba4e37f048ea3aeb61549
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 <11534@holbertonstudents.com>
2026-01-09 22:59:06 +0100

    task 8

---
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,40 @@
+import readDatabase from '../utils';
+
+export default class StudentsController {
+  static async getAllStudents(req, res) {
+    const database = process.argv[2];
+
+    try {
+      const fields = await readDatabase(database);
+
+      let response = 'This is the list of our students\n';
+
+      Object.keys(fields)
+        .sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()))
+        .forEach((field) => {
+          response += `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`;
+        });
+
+      res.status(200).send(response.trim());
+    } catch (error) {
+      res.status(500).send('Cannot load the database');
+    }
+  }
+
+  static async getAllStudentsByMajor(req, res) {
+    const database = process.argv[2];
+    const { major } = req.params;
+
+    if (major !== 'CS' && major !== 'SWE') {
+      res.status(500).send('Major parameter must be CS or SWE');
+      return;
+    }
+
+    try {
+      const fields = await readDatabase(database);
+      res.status(200).send(`List: ${fields[major].join(', ')}`);
+    } catch (error) {
+      res.status(500).send('Cannot load the database');
+    }
+  }
+}
